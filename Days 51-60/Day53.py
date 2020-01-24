ac9098c089c4e50e169981a19af5567041e8fffd
spiano <lafleurpiano@gmail.com>
2020-01-24 14:53:19 -0600

    day53 incomplete (more tests passing)

--- Days 51-60/Day53.py
+++ Days 51-60/Day53.py
@@ -27,40 +27,57 @@
 # requests = [(1, 4), (5, 9)]
 # min_num_taxis(requests) # => 1
 
-def sorted_requests(value):
-    return value[0]
+# I have decided to start over and go in a different direction
 
 def min_num_taxis(requests):
-    # After some failed tests, I am going to try to sort the values but I need a new function to do that
-    request_sort = requests.sort(key = sorted_requests)
-    sorted(requests)
-#     print(request_sort)
-    print(requests)
-
-    taxi_number = 1
-    # In order to make a comparison between entries, I will make a new array with all the values
-    requestArray = []
+    # before I start comparisons, I can eliminate edge cases
     # Unless empty, 1 taxi will always be required
-    for request in requests:
-        requestArray.append(request)
-        print(request)
+    if len(requests) == 0:
+        return 0
+    if len(requests) == 1:
+        return 1
+    # I am going to start by sorting the values but I need a new function to do that
+    # I realized I needed to sort by the second value in the pair instead of the first
+    # the searched resource: https://stackoverflow.com/questions/8459231/sort-tuples-based-on-second-parameter
+    # additional resource: https://stackoverflow.com/questions/5212870/sorting-a-python-list-by-two-fields
+    sort_request = sorted(requests, key=lambda x: (x[1], x[0]))
 
-#         Compare the first number (PO) of the second pair to the second number (DO) of the first pair.
-        if len(requestArray) > 1:
-            # Access the first number (PO) of each pair (I can do this by index number)
-             #If the DO is less than or equal to the PO increment the number of taxis needed by 1
-            if request[0] < requestArray[0][1]:
-                print('yes')
+    do = 0
+    pu = 1
+    pointer1 = sort_request[do][1] # points to the DO
+    pointer2 = sort_request[pu][0] # points to the PU
+    print(sort_request) # for testing purposes
+    # Now I can access the list with two separate pointers to compare the values
+    taxi_number = 1
+    # since the pointers are taking the pair index values I also need a move variable
+    move = 2
+    while move < len(requests):
+        print(str(move) + ' taxis required: ' + str(taxi_number))
+        print(pointer1, pointer2)
+        if pointer1 < pointer2:
+            pointer1 = sort_request[do + 1][1]
+            pointer2 = sort_request[pu + 1][0]
+            move += 1
+            do += 1
+            pu += 1
+        elif pointer1 >= pointer2:
+            taxi_number += 1
+            pointer2 = sort_request[pu + 1][0]
+            move += 1
+            pu += 1
+    if move == len(requests):
+            if pointer1 >= pointer2:
                 taxi_number += 1
-
-        #If many pairs, compare these numbers to determine if more taxis are needed
+            return taxi_number
     # return the total number of taxis needed for the request
     print(taxi_number)
     return taxi_number
 
 
 
-
-print(min_num_taxis([(1, 11), (1, 12), (3, 7), (3, 7), (4, 7), (5, 15), (5, 6), (10, 18), (11, 16), (11, 17), (13, 15), (16, 17), (16, 19), (17, 19), (19, 20)]))
+# print(min_num_taxis([(1,2)]))
+# print(min_num_taxis([(1, 4), (2, 9), (3, 6)]))
+print(min_num_taxis([(1, 4), (5, 9), (8, 9),(2, 3) ,(8, 9), (10, 12)]))
+# print(min_num_taxis([(1, 11), (1, 12), (3, 7), (2, 7), (4, 7), (5, 15), (5, 6), (10, 18), (11, 16), (11, 17), (13,15), (16, 17), (16, 19), (17, 19), (19, 20)]))
 
 
