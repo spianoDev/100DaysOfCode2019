d95768ad6802cf3425cef7da5ed4474e503a331f
spiano <lafleurpiano@gmail.com>
2020-02-20 10:10:04 -0600

    day72 fastest incomplete

--- Days 71-80/Day72.py
+++ Days 71-80/Day72.py
@@ -50,27 +50,51 @@
 
 # The above answer totally works, but it isn't efficient to pass all the tests
 
-def jumbled_string(s, n):
-    # everything gets wrapped in a recursive loop to run n times
-    while n > 0:
-    # I'm going to need two new variables to store the odd/even characters
-    # This time I am grabbing every other index using the python string range
-        even_front = s[0:len(s):2]
-        odd_back = s[1:len(s):2]
-        # Then concatenate the variables together
-        answer = even_front + odd_back
-#         print(answer)
-        s = answer
-        n -= 1
-        if n == 0:
-            print(answer)
-            return answer
+# def jumbled_string(s, n):
+#     # everything gets wrapped in a recursive loop to run n times
+#     while n > 0:
+#     # I'm going to need two new variables to store the odd/even characters
+#     # This time I am grabbing every other index using the python string range
+#         even_front = s[0:len(s):2]
+#         odd_back = s[1:len(s):2]
+#         # Then concatenate the variables together
+#         answer = even_front + odd_back
+# #         print(answer)
+#         s = answer
+#         n -= 1
+#         if n == 0:
+#             print(answer)
+#             return answer
 
+# jumbled_string("other example", 4) # "oeemeh altrxp"
 # this answer is much faster, but still times out
+
+def jumbled_string(s, n):
+    # I discovered that if I multiply n by 2 I will get the end order
+    # however, I need a way to iterate over the string until all the characters are included
+        step = 2**n
+        print(step)
+        step_again = 2**n # I need a second variable that won't mutate in recursive loop
+        new_order = s[0::step]
+    # Need a condition for if step is greater than the length of the string
+
+        # this recursive function will continue to add to the answer until step == 1 (index 1)
+        while step >= 1:
+            add_to_order = s[step-1::(step_again)]
+            step -= 1
+        # Then concatenate the variables together
+            answer = new_order + add_to_order
+            new_order = answer
+            if step == 1:
+                print(answer)
+                return answer
 
 # jumbled_string("Such Wow!", 1) # "Sc o!uhWw"
 # jumbled_string("qwertyuio", 2) # "qtorieuwy"
 # jumbled_string("better example", 2) # "bexltept merae"
-jumbled_string("Greetings", 8) # "Gtsegenri"
+jumbled_string("other example", 4) # "oeemeh altrxp"
+# jumbled_string("Greetings", 8) # "Gtsegenri"
+# jumbled_string("qwertyuio", 3) # "qoiuytrew"
 
-
+# I think I'm on the right track, but I need to move on in the interest of time
+# I don't see the pattern yet, so writing the code won't work 
