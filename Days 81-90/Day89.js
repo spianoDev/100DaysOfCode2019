edbcf4b18c368ec1cef22f76ed0b6bdb01fbb278
spiano <lafleurpiano@gmail.com>
2020-03-16 21:23:49 -0500

    day 89 incomplete

--- Days 81-90/Day89.js
+++ Days 81-90/Day89.js
@@ -26,7 +26,7 @@ function whoIsNext(names, r){
     console.log(r);
     let answer;
     //edge cases for r < or = names.length
-    if (r <= names.length) {
+    if (r < names.length) {
         answer = names[r-1];
         console.log(answer);
         return answer
@@ -35,12 +35,13 @@ function whoIsNext(names, r){
         // increases by 10. So, after first 5 people, you add 10, after that it's 20, and 30, and 40...
         let count = names.length * 2;
         r -= names.length;
-        while (r >= 0) {
+        while (r > 0) {
             r -= count;
             count += names.length * 2;
         }
-        if (Math.abs(r) <= names.length - 1) {
+        if (Math.abs(r) < names.length) {
             answer = names[r - 1];
+            console.log('this is the answer : ' + answer);
         // }
         // if (Math.abs(r) < names.length * names.length) {
         //     let index = Math.floor(Math.abs(r) / names.length);
@@ -52,12 +53,18 @@ function whoIsNext(names, r){
         } else {
             let index = Math.floor(Math.abs(r) / names.length);
             // if the index value is very high, need to keep dividing the value by 5 until you reach a number less than 5.
-            while (index >= names.length) {
+            while (index > names.length) {
                 index /= names.length;
                 console.log(index);
             }
-            // need to round down and that will return the correct value of the index.
-            index = Math.floor(index);
+            // if the index is an int, need to subtract 1 from the answer
+            if (Number.isInteger(index)) {
+                index -= 1;
+            } else {
+                // need to round down and that will return the correct value of the index.
+                index -= 1;
+                index = Math.floor(index);
+            }
             console.log('this is the index: ' + index);
             console.log('this is now r : ' + r);
             answer = names[index];
@@ -68,7 +75,10 @@ function whoIsNext(names, r){
 }
 
 // whoIsNext(["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 1); // "Sheldon"
-whoIsNext(["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 52); // "Penny"
+// whoIsNext(["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 52); // "Penny"
 // whoIsNext(["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 7230702951); // "Leonard"
+// whoIsNext(["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 6);
+whoIsNext([ 'Sheldon', 'Leonard', 'Penny', 'Rajesh', 'Howard' ], 1802);
 
-// I can pass the test cases, but I'm stumped on the wider tests.
+// I can pass the test cases, but I'm stumped on the wider tests. Tried some new things and I'm still stumped since
+// sometimes I'm off by one but other times I'm off more than that...
